d37884cc2b742c52bdf25ee43ecf95361cbbbb22
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-20 02:50:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each do |student, hash|
+      dude = self.new(hash)
+    end   
   end
 
   def add_student_attributes(attributes_hash)
